fc2d8616903ce3ff37d2ced8191aee9863149343
RazaGR <30994076+RazaGR@users.noreply.github.com>
2020-06-28 16:51:07 +0300

    Update and rename index.php to test.php

--- test.php
+++ test.php
@@ -5,11 +5,14 @@ $api = new \Supervisor\Api('127.0.0.1', 9001 /* username, password */);
 // Call Supervisor API
 echo  $api->getApiVersion()."\n";
 echo $api->twiddlerGetAPIVersion()."\n";
-print_r($api->twiddlerGetGroupNames()); 
+print_r($api->twiddlerGetGroupNames());
 echo "\Add program\n";
-$api->twiddlerAddProgramToGroup('razatest','ls');
-print_r($api->twiddlerGetGroupNames()); 
+$api->twiddlerAddProgramToGroup('dynamic','razatest','ls');
+print_r($api->twiddlerGetGroupNames());
 echo "\nRemove\n";
 $api->twiddlerRemoveProcessFromGroup('dynamic','razatest');
 echo "\nAfter remove\n";
 print_r($api->twiddlerGetGroupNames());
+
+
+
