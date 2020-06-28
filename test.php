a8cddd400871ab1673de4b38d25d5d29f7cd3c07
RazaGR <30994076+RazaGR@users.noreply.github.com>
2020-06-28 18:28:29 +0300

    Update test.php

--- test.php
+++ test.php
@@ -5,27 +5,24 @@ $api = new \Supervisor\Api('127.0.0.1', 9001 /* username, password */);
 // Call Supervisor API
 echo  $api->getApiVersion()."\n";
 echo $api->twiddlerGetAPIVersion()."\n";
-
+//$api->twiddlerRemoveProcessFromGroup('razalabs','razatest');
 echo "\Add program\n";
-$command = ['command' => 'ls -l','autostart' => 'false','autorestart' => 'false','startsecs' => '0'];
+$command = ['command' => 'ls -la /var/www/html','autostart' => 'false','autorestart' => 'false','startsecs' => '0'];
 $api->twiddlerAddProgramToGroup('razalabs','razatest',$command);
 
 print_r($api->startProcess('razalabs:razatest'));
 echo "\nSTART LOG\n";
 print_r($api->getProcessInfo('razalabs:razatest'));
 sleep(2);
-print_r($api->stopProcess('razalabs:razatest'));
-echo "\nSTOP LOG\n";
+#print_r($api->stopProcess('razalabs:razatest'));
+#echo "\nSTOP LOG\n";
 print_r($api->getProcessInfo('razalabs:razatest'));
 
 
 echo "\nPROGRAM LOG\n";
-print_r($api->readProcessLog('razalabs:razatest', 0, 50));
+print_r($api->readProcessLog('razalabs:razatest', 0, 500));
 
 echo "\nRemove\n";
 $api->twiddlerRemoveProcessFromGroup('razalabs','razatest');
 echo "\nAfter remove\n";
 print_r($api->twiddlerGetGroupNames());
-
-
-
