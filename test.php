b8963231f3b951081983adac69220a00debda08c
RazaGR <30994076+RazaGR@users.noreply.github.com>
2020-06-28 17:20:13 +0300

    Update test.php

--- test.php
+++ test.php
@@ -8,6 +8,7 @@ echo $api->twiddlerGetAPIVersion()."\n";
 print_r($api->twiddlerGetGroupNames());
 echo "\Add program\n";
 $command = json_encode(['command' => 'ls -l','autostart' => 'false','autorestart' => 'false','startsecs' => '0']);
+$command = 'ls -la';
 $api->twiddlerAddProgramToGroup('razalabs','razatest',$command);
 print_r($api->twiddlerGetGroupNames());
 echo "\nRemove\n";
